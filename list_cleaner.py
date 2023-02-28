c1148da99857ce76e9ec1f1e187605df71b40950
harboreast <126408668+harboreast@users.noreply.github.com>
2023-02-28 15:32:15 -0800

    Update list_cleaner.py

--- list_cleaner.py
+++ list_cleaner.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def ctl():
     keywords = ['.gov', '.png', 'jpg', 'png', 'set()', 'PNG', 'JPG', 'jpeg', 'JPEG', 'wixpress', '2x.png', '2x.jpg']
     scraped_email_addys = []
@@ -29,3 +32,41 @@ def ctl():
         for item in final_email_addys:
             # write each item on a new line
             fp.write("%s" % item)
+
+
+
+def first_url_check():
+    start_urls = []
+    urls = []
+    wcntr = 0
+    cntr = 0
+
+    f = open('input_urls.txt', 'r+')
+    f1 = f.readlines()
+
+    for xx in f1:
+        start_urls.append(xx.rstrip())
+
+    for xxx in start_urls:
+        try:
+            # Get Url
+            get = requests.get(xxx, verify=True, timeout=5)
+            # if the request succeeds
+            if get.status_code == 200:
+                print(f"{xxx}: is reachable,  {get.status_code}")
+                cntr += 1
+                urls.append(xxx)
+            else:
+                print(f"{xxx}: Failed. {get.status_code}")
+                wcntr += 1
+
+        # Exception
+        except requests.exceptions.RequestException as e:
+            # print URL with Errs
+            print(f"{xxx}: Failed.")
+            continue
+
+    print("Initially Loaded:", len(urls))
+    print(f"Working: {cntr} \n Failed: {wcntr}")
+    cntr = 0
+    return urls
